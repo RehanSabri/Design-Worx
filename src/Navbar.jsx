93ab2cc13aec92a7b3beb741054cea62cd552e6d
RehanSabri <iblamerehan@gmail.com>
2026-02-01 18:00:56 +0530

    Fixed navbar mobile view

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="mursee-nav">
-      
       {/* Left */}
       <div className="nav-logo-text">DW</div>
-      
+
       {/* Center */}
       <div className="nav-center">
         <a className="nav-link">DESIGN</a>
         <a className="nav-link">BUILD</a>
         <a className="nav-link">PMC</a>
       </div>
-      
+
       {/* Right */}
       <div>
         <button className="nav-btn">Work With Us</button>
       </div>
-
     </nav>
   );
 };
